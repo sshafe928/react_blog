20161a77d97c83afdc15aee783c386e1920f7711
Sabrina Shafer <141285407+sshafe928@users.noreply.github.com>
2025-01-10 14:19:03 -0700

    component set up

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,19 @@
 const Navbar = ({
-    
+    logo = "",
+    spotify = "",
+    pintrest = "",
+    pintrestLink = "",
+    spotifyLink = ""
 }) => {
     return (
         <>
+        <input type="text" id="search-bar" placeholder="Search..." />
+        <img src={logo} alt="Bare & Beautiful logo" />
+        <ul className="navbar-list">
+            <li><a href={spotifyLink}>{spotify}</a></li>
+            <li><a href={pintrestLink}>{pintrest}</a></li>
+            
+        </ul>
         </>
     );
 };
