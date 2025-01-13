fb19ea604ecdc2ca852ff8ae4ef3d9f5018477f4
Nico <141285419+red4cted25@users.noreply.github.com>
2025-01-13 14:28:34 -0700

    Completed Footer
    Completed footer with proper shadow, styling and sizing for everything.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,48 +4,48 @@ const Footer = ({
     profileImage2 = "https://res.cloudinary.com/dy2nnbnek/image/upload/v1736706239/download_zpzjh2.jpg",
     profileImage3 = "https://res.cloudinary.com/dy2nnbnek/image/upload/c_crop,w_500/v1736706240/Loose_Throw_Cheever_City_Cutout_Sweater_br7mjn.jpg",
     profileImage4 = "https://res.cloudinary.com/dy2nnbnek/image/upload/c_crop,w_500/v1736706243/American_Retro_Letter_Print_Oversized_Hoodie_Zip_Knit_Cardigan_ugswx4.jpg",
-    desc1 = "Fall outfit",
+    desc1 = "Fall Outfit",
     desc2 = "Dark Academia",
     desc3 = "Grunge",
     desc4 = "Oversized"
     
 }) => {
     return (
-        <>
+        <footer className="shadow-[rgba(0,0,15,0.5)_0px_-5px_4px_0px] bg-white">
         
-            <h2 class="text-center text-xl pb-10 ">Most Loved Posts</h2>
+            <h2 className="text-center text-xl py-10">Most Loved Posts</h2>
         
-            <div class="flex justify-center space-x-20 w-full">
-                <div class="flex flex-col items-center">
-                    <img class="w-60 h-60" src={profileImage1} alt={desc1} />
-                    <h2 class="text-center">{desc1}</h2>
+            <div className="flex justify-center space-x-20 w-full">
+                <div className="flex flex-col items-center">
+                    <img className="w-60 h-60" src={profileImage1} alt={desc1} />
+                    <h2 className="text-center">{desc1}</h2>
                 </div>
-                <div class="flex flex-col items-center">
-                    <img class="w-60 h-60" src={profileImage2} alt={desc2} />
-                    <h2 class="text-center">{desc2}</h2>
+                <div className="flex flex-col items-center">
+                    <img className="w-60 h-60" src={profileImage2} alt={desc2} />
+                    <h2 className="text-center">{desc2}</h2>
                 </div>
-                <div class="flex flex-col items-center">
-                    <img class="w-60 h-60" src={profileImage3} alt={desc3} />
-                    <h2 class="text-center">{desc3}</h2>
+                <div className="flex flex-col items-center">
+                    <img className="w-60 h-60" src={profileImage3} alt={desc3} />
+                    <h2 className="text-center">{desc3}</h2>
                 </div>
-                <div class="flex flex-col items-center">
-                    <img class="w-60 h-60" src={profileImage4} alt={desc4} />
-                    <h2 class="text-center">{desc4}</h2>
+                <div className="flex flex-col items-center">
+                    <img className="w-60 h-60" src={profileImage4} alt={desc4} />
+                    <h2 className="text-center">{desc4}</h2>
                 </div>
             </div>
 
 
         
-            <div class="flex justify-center w-full">
-                <h1 class="pr-10">JOIN OUR MAILING LIST</h1>
-                <form action="/" method="POST" class="flex items-center space-x-4 w-full">
-                    <input type="email" name="email" placeholder="Email" required class="block w-full sm:w-4/5 md:w-3/4 lg:w-1/2 p-2 border-2 border-black rounded-md focus:outline-none focus:ring-2 focus:ring-black"/>
-                    <button type="submit" class="p-2 bg-gray-600 text-white rounded-md hover:bg-gray-500 focus:outline-none"> Subscribe </button>
+            <div className="flex justify-center items-center w-full mt-20 mb-5">
+                <h1 className="pr-10">JOIN OUR MAILING LIST</h1>
+                <form action="/" method="POST" className="flex items-center w-2/5">
+                    <input type="email" name="email" placeholder="Email" required className="block w-full p-2 mr-2 border-[1px] border-black rounded-md focus:outline-none focus:ring-1 focus:ring-black"/>
+                    <button type="submit" className="p-2 bg-gray-600 text-center text-white rounded-md hover:bg-gray-500 focus:outline-none w-40"> Subscribe </button>
                 </form>
-                </div>
-            <p class="text-center text-xs">© COPYRIGHT 2025. BARE & BEAUTIFUL. ALL RIGHTS RESERVED. DESIGNED BY SABRINA & NICO</p>
+            </div>
+            <p className="text-center text-xs mb-1">© COPYRIGHT 2025. BARE & BEAUTIFUL. ALL RIGHTS RESERVED. DESIGNED BY SABRINA & NICO</p>
 
-        </>
+        </footer>
     );
 };
 
