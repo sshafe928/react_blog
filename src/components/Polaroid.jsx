5032a558bf04b540856f79c981ba740a334edd31
Sabrina Shafer <141285407+sshafe928@users.noreply.github.com>
2025-01-13 15:06:07 -0700

    button routing

--- src/components/Polaroid.jsx
+++ src/components/Polaroid.jsx
@@ -10,12 +10,12 @@ const PolaroidBoxes = () => {
     
         <div className="relative w-96 h-96 bg-white shadow-lg border-[15px] border-white overflow-hidden">
         <img src="https://res.cloudinary.com/dy2nnbnek/image/upload/v1736801834/Shirt_Design_N51107_Trending_Today_d7rm27.jpg" alt="Polaroid" className="w-full h-full object-cover" />
-        <button className="absolute w-4/5 bottom-36 left-1/2 transform -translate-x-1/2 px-4 py-4 bg-white text-black bg-opacity-80 focus:outline-none"> SHOP OUTFITS </button>
+        <a href="/shop"><button className="absolute w-4/5 bottom-36 left-1/2 transform -translate-x-1/2 px-4 py-4 bg-white text-black bg-opacity-80 focus:outline-none"> SHOP OUTFITS </button></a>
         </div>
 
         <div className="relative w-96 h-96 bg-white shadow-lg border-[15px] border-white overflow-hidden">
         <img src="https://res.cloudinary.com/dy2nnbnek/image/upload/v1736801834/Want_To_Build_a_Capsule_Wardrobe_in_2023__Start_Here_prol2t.jpg" alt="Polaroid" className="w-full h-full object-cover" />
-        <button className="absolute  w-4/5 bottom-36 left-1/2 transform -translate-x-1/2 px-4 py-4 bg-white text-black bg-opacity-80 focus:outline-none" > LOVED POSTS & SUBSCRIBE </button>
+        <a href="#footer"><button className="absolute  w-4/5 bottom-36 left-1/2 transform -translate-x-1/2 px-4 py-4 bg-white text-black bg-opacity-80 focus:outline-none" > LOVED POSTS & SUBSCRIBE </button></a>
         </div>
     </div>
     );
