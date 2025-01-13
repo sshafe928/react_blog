216c33108e85383fb32ed2d109ce52f371fe46eb
Sabrina Shafer <141285407+sshafe928@users.noreply.github.com>
2025-01-13 14:49:14 -0700

    postcards and star man

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -8,8 +8,10 @@ function Main() {
   <>
   <div className="bg-gray-200">
     <Navbar/>
-
-    <h1 class="p-16">Bare & Beautiful</h1>
+    <img className="absolute left-12 max-w-40 top-28" src="https://res.cloudinary.com/dy2nnbnek/image/upload/v1736804643/Screenshot__64_-removebg-preview_mzgu51.png" alt="Star man" />
+    <div className="flex justify-center text-4xl">
+      <h1 class="p-20">Bare & Beautiful</h1>
+    </div>
     <PolaroidBoxes/>
     <Footer/>
   </div>
