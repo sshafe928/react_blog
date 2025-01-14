32b4074298ac4c4905d39178da4aff1d4f495ddc
Sabrina Shafer <sshafe928@west-mec.org>
2025-01-14 10:58:54 -0700

    design

--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -6,7 +6,7 @@ const ShopItem = ({ Outfit }) => {
     return (
         <>
         <div className="absolute inset-0 bg-cover bg-center filter blur-lg opacity-5" style={{backgroundImage: "url('https://res.cloudinary.com/dy2nnbnek/image/upload/v1736876998/165acab1-3b95-4c24-9187-c78ed79781da_cowv2w.jpg')", zIndex: -1 }}></div>
-            <div className="flex mb-36 mt-12">
+            <div className="flex  justify-center items-center min-h-screen">
                 
                 <div className="w-1/4 pr-4">
                     <h2>{mainTitle}</h2>
